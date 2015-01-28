d29f136750641e9b5eb436d38a686375d779154d
QIU Ning <pkuqiuning@gmail.com>
2015-01-27 20:57:12 -0500

    part1 works

--- 2-R/assign1/pollutantmean.R
+++ 2-R/assign1/pollutantmean.R
@@ -11,4 +11,23 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
     
     ## Return the mean of the pollutant across all monitors list
     ## in the 'id' vector (ignoring NA values)
+    
+    # read in files
+    files_list <- list.files(directory, full.names=TRUE)
+    csvfiles = lapply(files_list[id], read.csv)
+    
+    # merge data
+    data = do.call(rbind, csvfiles)
+    mean(data[[pollutant]], na.rm=T)
 }
+
+
+
+library(testthat)
+ERROR = 0.001
+test_that('part1 is correct', {
+    expect_less_than(pollutantmean("data/specdata", "sulfate", 1:10) - 4.064, ERROR)
+    expect_less_than(pollutantmean("data/specdata", "nitrate", 70:72) - 1.706, ERROR)
+    expect_less_than(pollutantmean("data/specdata", "nitrate", 23) - 1.281, ERROR)
+    
+})
