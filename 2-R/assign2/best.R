c7e5dc7d6fec6de8a0509c5a21bf0033c6ecc2f1
QIU Ning <pkuqiuning@gmail.com>
2015-01-28 14:55:43 -0500

    write is? function

--- 2-R/assign2/best.R
+++ 2-R/assign2/best.R
@@ -2,12 +2,21 @@
 
 
 OUTCOME_FILE = "outcome-of-care-measures.csv"
-
-
 ERROR_INV_STATE = 'invalid state'
 ERROR_INV_OUTCOME = 'invalid outcome'
 
+
 # Data
+
+# State is one of 
+STATES_LIST_STR = "AK,AL,AR,AZ,CA,CO,CT,DC,DE,FL,GA,GU,HI,IA,ID,IL,IN,KS,KY,LA,MA,MD,ME,MI,MN,MO,MS,MT,NC,ND,NE,NH,NJ,NM,NV,NY,OH,OK,OR,PA,PR,RI,SC,SD,TN,TX,UT,VA,VI,VT,WA,WI,WV,WY"
+STATES_LIST = unlist(strsplit(STATES_LIST_STR, ','))
+# interp. two letter abbreviation the states in the US
+is.State = function(state) state %in% STATES_LIST
+
+# STATES_LIST = sort(unique(data$State))
+# paste(STATES_LIST, collapse = ',')
+
 
 # Condition is one of
 CONDITION_LIST = list(
@@ -16,18 +25,11 @@ CONDITION_LIST = list(
     "pneumonia"
 )
 # interp. the clinical condition
+is.Condition = function(cond) cond %in% CONDITION_LIST
 
 MORTALITY_COL_LOOKUP = c(11,17,23)
 names(MORTALITY_COL_LOOKUP) = CONDITION_LIST
 
-# State is one of "NJ", "NY"...
-# interp. two letter abbreviation the states in the US
-
-#
-# STATES_LIST = sort(unique(data$State))
-# paste(STATES_LIST, collapse = ',')
-STATES_LIST_STR = "AK,AL,AR,AZ,CA,CO,CT,DC,DE,FL,GA,GU,HI,IA,ID,IL,IN,KS,KY,LA,MA,MD,ME,MI,MN,MO,MS,MT,NC,ND,NE,NH,NJ,NM,NV,NY,OH,OK,OR,PA,PR,RI,SC,SD,TN,TX,UT,VA,VI,VT,WA,WI,WV,WY"
-STATES_LIST = unlist(strsplit(STATES_LIST_STR, ','))
 
 # Methods
 
@@ -35,8 +37,8 @@ STATES_LIST = unlist(strsplit(STATES_LIST_STR, ','))
 # State, Condition -> string
 best <- function(state, outcome) {
     ## Check that state and outcome are valid
-    if (! outcome %in% CONDITION_LIST) stop(ERROR_INV_OUTCOME)
-    if (! state %in% STATES_LIST) stop(ERROR_INV_STATE)
+    if (! is.Condition(outcome)) stop(ERROR_INV_OUTCOME)
+    if (! is.State(state)) stop(ERROR_INV_STATE)
    
     mort_col = MORTALITY_COL_LOOKUP[[outcome]]
     
